033016237313c7b622034b7dca206ffc3c0dfa2a
lszredtea <lszredtea@163.com>
2021-09-22 17:42:21 +0800

    2 commit

---
+++ app/userreg.php
@@ -0,0 +1,29 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>用户登录页</title>
+    <style>
+        #container{
+            margin: 50px auto;
+        }
+    </style>
+</head>
+<body>
+<div id="container">
+    <div>
+    <label>用户名</label>
+<input type="text" name="username" placeholder="user name">
+    </div>
+    <div>
+   <label>密码</label>
+<input type="password" name="pwd"  >
+    </div>
+</div>
+</body>
+</html>
